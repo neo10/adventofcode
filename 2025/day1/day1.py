749cc76f875706455881401a7d236796d2c09b7d
Philipp Baars <atlas5@posteo.de>
2025-12-08 00:51:37 +0100

    improvements

--- 2025/day1/day1.py
+++ 2025/day1/day1.py
@@ -1,60 +1,45 @@
-went_over_0 = 0
 
 with open('2025/day1/day1.txt', 'r', encoding='utf-8') as f:
     zeilen_liste = f.readlines()
 
-print(zeilen_liste[0])
-
-# 1. Nur 'def', kein 'function'
-# 2. Doppelpunkt am Ende
-def handle_zeile(zeile: str, position: int):
-    # 3. Zuerst 'match' auf die Variable anwenden
-    match zeile[0]:
+def handle_line(line:str, startpos:int)-> tuple[int,int]:
+    match line[0]:
         case "L":
-            newPosition = position - int(getNumber(zeile))
-            return get_valid_position(newPosition)
-
+            move = - int(line[1:])
+            return get_pos(startpos,move), overflows(startpos,move)
         case "R":
-            newPosition = position + int(getNumber(zeile))
-            return get_valid_position(newPosition)
+            move = int(line[1:])
+            return get_pos(startpos,move), overflows(startpos,move)
 
         case _:
             print("ERROR: Erster Buchstabe weder R noch L")
 
 
 
-
-def getNumber(zeile):
-    return zeile[1:]
-
-# for zeile in zeilen_liste:
-#     print(getNumber(zeile))
-
-def get_valid_position(position:int) -> int:
-    # print("Aufgerufen mit Position= ", position)
-    if position <=99 and position >=0:
-        return position
-    global went_over_0
-    went_over_0 +=1
-    if position < 0:
-        return get_valid_position(position + 100)
-    elif position > 99:
-        return get_valid_position(position - 100)
+def get_pos(pos,move) -> int:
+    rawPos = pos + move
+    return rawPos % 100
+def overflows(pos, move)->int:
+    rawPos = pos + move
+    if rawPos < 0:
+        return 1 + (abs(rawPos) // 100)
+    else:
+        return rawPos // 100
 
 #Teil1
 solution = 0
 pos = 50
 for zeile in zeilen_liste:
-    pos = handle_zeile(zeile, pos)
+    overflows = 0
+    pos, overflows = handle_line(zeile,pos)
+    print("New Pos: ",pos)
     if pos == 0:
         solution += 1
+    went_over_0 += overflows
 
-print(get_valid_position(-1))
 
-print("Lösung: ", solution)
-print("Lösung: ", solution + went_over_0)
+print("Lösung Genau 0: ", solution)
+print("Lösung Genau 0 + Overflows: ", solution + went_over_0)
 
-# test_zeile = "L1000"
+print(handle_line("R1000",50))
 
-# print(handle_zeile(test_zeile,50))
-# print(went_over_0)
--- 2025/day1/test_day1.py
+++ 2025/day1/test_day1.py
@@ -1,80 +1,59 @@
 import unittest
 import day1
-
-def handle_line(line:str, startpos:int)-> tuple[int,int]:
-    match line[0]:
-        case "L":
-            newPosition = startpos - int(line[1:])
-            return get_real_pos(newPosition),
-
-        case "R":
-            newPosition = startpos + int(line[1:])
-            return get_real_pos(newPosition)
-
-        case _:
-            print("ERROR: Erster Buchstabe weder R noch L")
-
-
-
-def get_real_pos(pos):
-    if pos < 0:
-        return 100 -(-pos % 100)
-    elif pos > 99:
-        return (pos % 100)
-    else:
-        return pos
-def get_div_amount(pos):
-    if pos >= 0 or pos <= 99:
-        return pos
-    else:
-        return (abs(pos) // 100)  
+   
 
 
 # Das ist der Test-Block
 class MeinTest(unittest.TestCase):
     def test_1(self):
-        line = "L30"
-        startpos = 20
-        expected = 90,
-        result = handle_line(line,startpos)
+        startpos = 1
+        expected = 0
+        result = day1.get_pos(startpos,-1)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_2(self):
-        line = "R30"
-        startpos = 20
-        expected = 50,
-        result = handle_line(line,startpos)
+        startpos = 1
+        expected = 99
+        result = day1.get_pos(startpos,-2)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_3(self):
-        line = "R30"
-        startpos = 80
-        expected = 10,
-        result = handle_line(line,startpos)
+        startpos = 50
+        expected = 50
+        result = day1.get_pos(startpos,1000)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_4(self):
-        line = "L200"
-        startpos = 0
+        startpos = 35
         expected = 0
-        result = handle_line(line,startpos)
+        result = day1.get_pos(startpos,65)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
-
+    
+    
+    
+    
     def test_5(self):
-        line = "L200"
-        startpos = 1
+        startpos = 35
         expected = 1
-        result = handle_line(line,startpos)
+        result = day1.overflows(startpos,65)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_6(self):
-        expected = 2
-        result = get_div_amount(200)
+        startpos = 10
+        expected = 0
+        result = day1.overflows(startpos,-10)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_7(self):
+        startpos = 10
         expected = 1
-        result = get_div_amount(150)
+        result = day1.overflows(startpos,-11)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
     def test_8(self):
+        startpos = 10
         expected = 2
-        result = get_div_amount(-250)
+        result = day1.overflows(startpos,200)
         self.assertEqual(result, expected, f"Fehler! Erwartet: {expected}, aber erhalten: {result}")
+    def test_9(self):
+        for zeile in day1.zeilen_liste:
+            pos, overflows = day1.handle_line(zeile)
+            self.assertFalse(pos < 0 or pos > 99,"Position außerhalb von 0-99!")
+   
 
 # Startet die Tests, wenn man die Datei ausführt
 if __name__ == '__main__':
