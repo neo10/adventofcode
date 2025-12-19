b4db8fdb50dd7d50c94e3683addbb76231dddbb6
Philipp Baars <philipp.baars@mail.schwarz>
2025-12-19 16:04:02 +0100

    simple

---
+++ 2025/day2/day2.py
@@ -0,0 +1,5 @@
+with open('2025/day2/test.txt', 'r', encoding='utf-8') as f:
+    zeilen_liste = f.readlines()
+
+
+
---
+++ 2025/day2/test_day2.py
@@ -0,0 +1,6 @@
+import unittest
+import day2
+
+class TestSimple(unittest.TestCase):
+
+    def test_simple(self):
