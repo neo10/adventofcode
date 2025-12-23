e4dffb4b70702ffee6f3d055ccb57abc6704f897
Philipp Baars <philipp.baars@mail.schwarz>
2025-12-23 10:11:44 +0100

    day2 part1 solved

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,5 +1,40 @@
-with open('2025/day2/test.txt', 'r', encoding='utf-8') as f:
-    zeilen_liste = f.readlines()
+with open('./data.txt', 'r', encoding='utf-8') as f:
+    input = f.read().strip()
 
+bereiche = []
+for teil in input.split(','):
+    a, b = teil.split('-')
+    bereiche.append([int(a), int(b)])
+
+
+def is_digits_repeated_twice(number)-> bool:
+    anzahl_ziffern = len(str(abs(number)))
+    if anzahl_ziffern % 2 != 0:
+        return False
+
+    s = str(abs(number))
+    halb = anzahl_ziffern // 2
+
+    first_half = s[:halb]
+    second_half = s[halb:]
+
+    if first_half == second_half:
+        return True
+    else:
+        return False
+
+solution1 = 0 
+for bereich in bereiche:
+    try:
+        num1 = bereich[0]
+        num2 = bereich[1]
+    except ValueError:
+        continue
+    numbers = range(num1,num2+1)
+    for number in numbers:
+        if is_digits_repeated_twice(number):
+            solution1 += number
+    
+print("Lösung1: ",solution1)
 
 
--- 2025/day2/test_day2.py
+++ 2025/day2/test_day2.py
@@ -4,3 +4,13 @@ import day2
 class TestSimple(unittest.TestCase):
 
     def test_simple(self):
+        actual_numbers = []
+        expected_numbers = [11,22,99,1010,1188511885,222222,446446,38593859,]
+        for bereich in day2.bereiche:
+            numbers = range(bereich[0],bereich[1]+1)
+            for number in numbers:
+                if day2.is_digits_repeated_twice(number):
+                    actual_numbers.append(number)
+        self.assertListEqual(expected_numbers,actual_numbers)
+        print(actual_numbers)
+        
