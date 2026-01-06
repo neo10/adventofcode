0bbf8439ed7db2768d8b5dab05d878791a97e366
Philipp Baars <atlas5@posteo.de>
2026-01-06 23:42:22 +0100

    day4 init

---
+++ 2025/day4/day4.py
@@ -0,0 +1,24 @@
+input_array = []
+
+with open('./test.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip()            
+        zeichen = [str(c) for c in line] 
+        input_array.append(zeichen)
+
+
+
+max_index = len(input_array)-1
+for i in range(len(input_array)):
+    top_index = i-1 if i > 0 else 0
+    bottom_index = i+1 if i < max_index else i
+
+    for k in range(len(input_array[i])):
+        max_index2 = len(input_array[i])-1
+        left_index = k-1 if k > 0 else 0
+        right_index = k+1 if k < max_index2 else k
+
+        
+
+        
+    
