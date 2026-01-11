093261b5edb80bf18df8ee00a442b6513c8edb7d
Philipp Baars <atlas5@posteo.de>
2026-01-12 00:41:52 +0100

    neighbors

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -7,7 +7,13 @@ with open('./test.txt', 'r', encoding='utf-8') as f:
         input_array.append(zeichen)
 
 
+def is_roll_of_paper(array:list[str],index_y:int,index_x:int)->bool:
+    if array[index_y][index_x] == '@':
+        return True
+    
+    return False
 
+indexes = []
 max_index = len(input_array)-1
 for i in range(len(input_array)):
     top_index = i-1 if i > 0 else 0
@@ -17,6 +23,9 @@ for i in range(len(input_array)):
         max_index2 = len(input_array[i])-1
         left_index = k-1 if k > 0 else 0
         right_index = k+1 if k < max_index2 else k
+
+neighbor_indexes = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+
 
         
 
