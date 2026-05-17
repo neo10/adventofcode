e79d5508fa0aef388fa0c12f7baa4e346017ade2
Philipp Baars <atlas5@posteo.de>
2026-05-17 23:36:16 +0200

    progress

--- 2025/day6/day6_2.py
+++ 2025/day6/day6_2.py
@@ -9,14 +9,35 @@ def get_grid(lines: list[str]) -> list[str]:
     return [line.ljust(width, " ") for line in lines]
 
 
+def get_max_digits(grid: list[str]) -> int:
+    result = 0
+    counter = 0
+    for line in grid:
+        for elem in line:
+            if elem.isdigit():
+                counter += 1
+                if counter > result:
+                    result = counter
+            else:
+                counter = 0
+    return result
+
+
+def get_grid_array(grid: list[str], numberLength: int) -> list[list[str]]:
+    result: list[list[str]] = []
+    for x in range(len(grid)):
+        lineList: list[str] = []
+        for i in range(0, len(grid[x]), numberLength + 1):
+            number = grid[x][i : i + numberLength]
+            lineList.append(number)
+        result.append(lineList)
+
+    return result
+
+
 lines = load_data("test.txt")
-print(lines)
-
 grid = get_grid(lines)
-print(grid)
-for line in grid:
+maxDigits = get_max_digits(grid)
+grid_array = get_grid_array(grid, maxDigits)
+for line in grid_array:
     print(line)
-    for elem in line:
-        print(elem)
-
-# Build Blocks
