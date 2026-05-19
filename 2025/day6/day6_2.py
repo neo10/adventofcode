928deb9d5a2435a2db3daf5f258595cbe0bbfad0
Philipp Baars <atlas5@posteo.de>
2026-05-20 00:57:03 +0200

    error fix

--- 2025/day6/day6_2.py
+++ 2025/day6/day6_2.py
@@ -9,6 +9,23 @@ def get_grid(lines: list[str]) -> list[str]:
     return [line.ljust(width, " ") for line in lines]
 
 
+def calculate_solution(numbers: list[int], calcSymbol: str) -> int:
+    print(calcSymbol)
+    match calcSymbol:
+        case "+":
+            result = 0
+            for number in numbers:
+                result += number
+        case "*":
+            result = 1
+            for number in numbers:
+                result *= number
+        case _:
+            raise ValueError(f"Unbekannter Operator: {calcSymbol}")
+
+    return result
+
+
 def get_max_digits(grid: list[str]) -> int:
     result = 0
     counter = 0
@@ -25,12 +42,47 @@ def get_max_digits(grid: list[str]) -> int:
 
 def get_grid_array(grid: list[str], numberLength: int) -> list[list[str]]:
     result: list[list[str]] = []
-    for x in range(len(grid)):
+    print(grid)
+    for x in range(len(grid[0])):
+        headerChar = grid[0][x]
+        print(f"checking for value={headerChar}")
+        if headerChar == " ":
+            border = True
+            for y in range(len(grid)):
+                print(f"x={x}, y={y}, value={grid[y][x]}")
+                if grid[y][x] != " ":
+                    border = False
+                    break
+            if border:
+                print("BORDER")
         lineList: list[str] = []
-        for i in range(0, len(grid[x]), numberLength + 1):
-            number = grid[x][i : i + numberLength]
-            lineList.append(number)
-        result.append(lineList)
+
+    return result
+
+
+def getCalcSymbol(inputString: str) -> str:
+    for char in inputString:
+        if char == "*":
+            return "*"
+        elif char == "+":
+            return "+"
+
+
+def get_solution(grid_array: list[list[str]], maxDigits: int) -> int:
+    result = 0
+    for x in range(len(grid_array[0])):
+        numbers: list[int] = []
+        for num in range(maxDigits):
+            number_chars = ""
+            for y in range(len(grid_array)):
+                char = grid_array[y][x][num]
+                if char.isdigit():
+                    number_chars += char
+
+            if number_chars:
+                numbers.append(int(number_chars))
+        calcSymbol = getCalcSymbol(grid_array[-1][x])
+        result += calculate_solution(numbers, calcSymbol)
 
     return result
 
@@ -39,5 +91,3 @@ lines = load_data("test.txt")
 grid = get_grid(lines)
 maxDigits = get_max_digits(grid)
 grid_array = get_grid_array(grid, maxDigits)
-for line in grid_array:
-    print(line)
