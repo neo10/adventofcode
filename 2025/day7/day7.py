39a3e7138526f89725636b1707d8bb5bd683cf5e
Philipp Baars <atlas5@posteo.de>
2026-05-31 21:09:19 +0200

    solution day7 part2

--- 2025/day7/day7.py
+++ 2025/day7/day7.py
@@ -27,10 +27,35 @@ def count_splits(positions_now: list[int], grid: list[list[str]], index_row: int
     return counter + count_splits(positions, grid, index_row + 1)
 
 
+def count_timelines(grid: list[list[str]]) -> int:
+    start_index = grid[0].index("S")
+    positions: dict[int, int] = {start_index: 1}
+    counter = 1
+    for row in grid:
+        next_positions: dict[int, int] = {}
+        for position, amount in positions.items():
+            if position < 0 or position >= len(row):
+                continue
+            if row[position] == "^":
+                counter += amount
+                newPos1 = position - 1
+                newPos2 = position + 1
+                next_positions[newPos1] = next_positions.get(newPos1, 0) + amount
+                next_positions[newPos2] = next_positions.get(newPos2, 0) + amount
+            else:
+                next_positions[position] = next_positions.get(position, 0) + amount
+
+        positions = next_positions
+
+    return counter
+
+
 grid = load_data("data.txt")
 
 start_index = grid[0].index("S")
 
 solution = count_splits([start_index], grid, 0)
+solution2 = count_timelines(grid)
 
 print(f"Lösung1={solution}")
+print(f"Lösung2={solution2}")
